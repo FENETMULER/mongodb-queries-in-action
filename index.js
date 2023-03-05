bad73d9ac7dbffd8a55bfeda5f62e124d0d3cb07
FENET <productivefenetmuler@gmail.com>
2023-03-05 12:31:05 +0300

    add sample outputs to find queries as comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const users = require("./user.mongo");
 const mongoConnect = require("./services");
 
+//
+
 mongoConnect();
 
 //****OPERATIONS****
@@ -8,17 +10,31 @@ mongoConnect();
 //**FIND**
 
 //Without projection
-const findUser = async (name) => {
+const findUser = async () => {
   const result = await users.find({ name: "Hortense" });
+
   console.log(result);
+  /* 
+  {
+    _id: new ObjectId("61f0518e4385bfe13cdf3c54"),
+    name: 'Hortense',
+    email: 'hbenford7@so-net.ne.jp',
+    gender: 'Female',
+    age: 8
+  }
+  */
 };
 
 //With projection - Inclusion
 const findUser2 = async () => {
   const result = await users.find(
     { name: "Hortense" },
-    { name: 1, age: 1, _id: 0, email: 0 }
+    { name: 1, age: 1, _id: 0 }
   );
+
+  console.log(result);
+
+  //{ name: 'Hortense', age: 8 }
 };
 
 //with projection - Exclusion
@@ -28,6 +44,8 @@ const findUser3 = async () => {
     { email: 0, gender: 0, _id: 0 }
   );
   console.log(result);
+
+  //{ name: 'Hortense',, age: 8 }
 };
 
 //using comparison operator
@@ -36,7 +54,28 @@ const findUser4 = async () => {
   const result = await users.find({
     age: { $lt: 21 },
   });
+
   console.log(result);
+
+  /*
+  [
+  {
+    _id: new ObjectId("61f0518e4385bfe13cdf3c4d"),
+    name: 'Liam',
+    email: 'lbyneth0@tripod.com',
+    gender: 'Female',
+    age: 8
+  },
+  {
+    _id: new ObjectId("61f0518e4385bfe13cdf3c50"),
+    name: 'Haven',
+    email: 'hmaundrell3@dailymotion.com',
+    gender: 'Male',
+    age: 4
+  },
+  ...
+]
+  */
 };
 
 findUser4();
